6e373c659485aac668fa19518557f34ac22b7e8a
MilanBhanuka <vgmbhanuka@gmail.com>
2024-08-07 21:50:24 +0530

    commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+
+import React from 'react'
+import Navbar from './components/Navbar/navbar'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar/navbar.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div className=''>
+      
+    </div>
+  )
+}
+
+export default navbar
